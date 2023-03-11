a421ed611c42adbc3a4b83a8d60a9fc14cf7e2c0
athulpm <97athulpm@gmail.com>
2023-03-11 23:14:36 +0530

    Improved work details page

--- src/components/work/WorkDetails.js
+++ src/components/work/WorkDetails.js
@@ -1,21 +1,24 @@
 import { useContext } from "react";
-import { useParams } from "react-router"
+import { Container } from "react-bootstrap";
+import { useParams } from "react-router";
 import ContentContext from "../../store/content-context";
 
 const WorkDetails = () => {
-    //params
-    const {workId} = useParams();
+  //params
+  const { workId } = useParams();
 
-    //store
-    const contentCtx = useContext(ContentContext);
-    const work = contentCtx.work;
+  //store
+  const contentCtx = useContext(ContentContext);
+  const work = contentCtx.work;
 
-    const workToDisplay = work.find((item) => item.id === workId);
+  const workToDisplay = work.find((item) => item.id === workId);
 
-    return <div>
-        <h1>{workToDisplay.description}</h1>
-        <img src={workToDisplay.url} />
-    </div>
-}
+  return (
+    <Container style={{textAlign:'center'}}>
+      <h1>{workToDisplay.description}</h1>
+      <img src={workToDisplay.url} alt={workToDisplay.description} style={{maxWidth:'100%', height:'auto'}} />
+    </Container>
+  );
+};
 
 export default WorkDetails;
