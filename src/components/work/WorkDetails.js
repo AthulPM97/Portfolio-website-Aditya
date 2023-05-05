a94d462e0dc51678ae189a2e7b037ba04d01ea12
athulpm <97athulpm@gmail.com>
2023-05-05 17:35:26 +0530

    Added more description to work details

--- src/components/work/WorkDetails.js
+++ src/components/work/WorkDetails.js
@@ -3,6 +3,8 @@ import { Container, Image } from "react-bootstrap";
 import { useParams } from "react-router";
 import ContentContext from "../../store/content-context";
 
+const dummyText = ['This is the first dummy text under one image', 'This is the first dummy text under one image', 'This is the first dummy text under one image']
+
 const WorkDetails = () => {
   //params
   const { workId } = useParams();
@@ -16,14 +18,23 @@ const WorkDetails = () => {
   return (
     <Container style={{ textAlign: "center" }}>
       <h1>{workToDisplay.description}</h1>
-      <Container>
+      <div style={{boxSizing: 'border-box'}}>
+        <h4>{dummyText[0]}</h4>
         <Image
           src={workToDisplay.url}
           thumbnail
-          // style={{ maxWidth: "100%", height: "auto" }}
+          style={{ maxWidth: "100%", height: "auto" }}
           alt={workToDisplay.description}
         />
-      </Container>
+        <h4>{dummyText[1]}</h4>
+        <Image
+          src={workToDisplay.url}
+          thumbnail
+          style={{ maxWidth: "100%", height: "auto" }}
+          alt={workToDisplay.description}
+        />
+        <h4>{dummyText[2]}</h4>
+      </div>
     </Container>
   );
 };
